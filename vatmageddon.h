5e8a5efeeebffefe1f2948ecf5fab6f1303f5fee
Andrew Clayton <andrew@digital-domain.net>
2011-01-28 11:46:15 +0000

    Update the cb_calculate function declaration in the header file
    Update it to include the function parameters to match what's in
    the .c file.

--- vatmageddon.h
+++ vatmageddon.h
@@ -12,7 +12,7 @@
 static void cb_quit();
 static void cb_about();
 static void cb_reset(GtkWidget *widget, gpointer data);
-static void cb_calculate();
+static void cb_calculate(GtkWidget *widget, gpointer data);
 static double do_rounding(double to_round);
 static void calculate_gross();
 static void calculate_net();
